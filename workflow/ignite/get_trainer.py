ca9e0c4f2e294cf281f41d3fd331a880e8cfe960
Richard <samedii@gmail.com>
2020-01-26 22:17:27 +0100

    Prefer dict.get #9

--- workflow/ignite/get_trainer.py
+++ workflow/ignite/get_trainer.py
@@ -8,23 +8,20 @@ def get_trainer(model, criterion, optimizer, config, track_loss=True):
     def train_step(engine, batch):
 
         model.train()
-        accumulation_steps = (
-            1 if 'accumulation_steps' not in config
-            else config['accumulation_steps']
-        )
+        accumulation_steps = config.get('accumulation_steps', 1)
 
         if engine.state.iteration % accumulation_steps == 0:
             optimizer.zero_grad()
 
         batch = workflow.torch.batch_to_model_device(batch, model)
-        if 'mixup_alpha' in config and config['mixup_alpha'] > 0:
+        if 'mixup_alpha' in config:
             batch = workflow.torch.mixup_batch(batch, alpha=config['mixup_alpha'])
         output = model(batch['features'])
         loss = criterion(output, batch['targets']) / accumulation_steps
         loss.backward()
 
         if engine.state.iteration % accumulation_steps == 0:
-            if 'clip_norm' in config and config['clip_norm']:
+            if config.get('clip_norm', False):
                 torch.nn.utils.clip_grad_norm_(
                     model.parameters(), config['clip_norm']
                 )
