572509137abece7ab3153dfc7d933b5de9789848
Richard <samedii@gmail.com>
2020-01-26 22:51:22 +0100

    Change to 4 space indentation

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup, find_packages
 
 
 setup(
-   name='ml-workflow',
-   version='0.1.1',
-   description='Workflow tools for pytorch and ignite',
-   author='Felix Abrahamsson, Richard Löwenström, Jim Holmström',
-   author_email='richard@aiwizo.com',
-   keywords='pytorch ignite workflow utilities',
-   packages=find_packages(exclude=['venv', 'dist']),
-   install_requires=[
+    name='ml-workflow',
+    version='0.1.1',
+    description='Workflow tools for pytorch and ignite',
+    author='Felix Abrahamsson, Richard Löwenström, Jim Holmström',
+    author_email='richard@aiwizo.com',
+    keywords='pytorch ignite workflow utilities',
+    packages=find_packages(exclude=['venv', 'dist']),
+    install_requires=[
         'torch>=1.3.1',
         'ignite>=0.3.0',
         'tqdm>=4.41.1',
         'opencv-python>=4.1.2.30',
         'pandas>=0.25.3',
-   ],
+    ],
 )
