c9fbaad7074314ccc03cbe932b56e279a2e4ea99
Felix Abrahamsson <felix@aiwizo.com>
2020-01-27 09:46:13 +0100

    Move helper function get_layer_output_size

--- workflow/torch/__init__.py
+++ workflow/torch/__init__.py
@@ -1,6 +1,5 @@
 from .batch_to_model_device import batch_to_model_device
 from .get_conv_output_size import get_conv_output_size
-from .get_layer_output_size import get_layer_output_size
 from .get_model_summary import get_model_summary
 from .initialize_weights import initialize_weights
 from .load_best_model import load_best_model
