013fc44948fe524395564ffe13a1f6b74c6b2bb6
Felix Abrahamsson <felix@aiwizo.com>
2020-01-27 09:45:02 +0100

    batch_to_model_device now accepts all input types

--- workflow/torch/batch_to_model_device.py
+++ workflow/torch/batch_to_model_device.py
@@ -2,8 +2,4 @@ from workflow.torch.to_device import to_device
 
 
 def batch_to_model_device(batch, model):
-    device = next(model.parameters()).device
-    if type(batch) in (list, dict):
-        return to_device(batch, device)
-    else:
-        raise ValueError('Expected batch to be type list or dict')
+    return to_device(batch, next(model.parameters()).device)
