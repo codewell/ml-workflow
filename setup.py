b829ac542696136e6478c0b752c93de28deb897b
Richard <samedii@gmail.com>
2020-01-30 17:44:37 +0100

    Bump version v0.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ml-workflow',
-    version='0.2.1',
+    version='0.2.2',
     description='Workflow tools for pytorch and ignite',
     author='Felix Abrahamsson, Richard Löwenström, Jim Holmström',
     author_email='richard@aiwizo.com',
