14dea788064c4119790540684858d4b2e008cfd8
Richard <samedii@gmail.com>
2020-01-31 12:37:49 +0100

    Add optional length to sampler

--- workflow/torch/iterable_sampler.py
+++ workflow/torch/iterable_sampler.py
@@ -1,13 +1,19 @@
 import torch
+from itertools import islice
 
 
 class IterableSampler(torch.utils.data.Sampler):
-    def __init__(self, iterable):
+    def __init__(self, iterable, length=None):
         super().__init__(data_source=None)
         self.iterable = iterable
+        self.iterator = iter(iterable)
+        self.length = length
 
     def __iter__(self):
-        return iter(self.iterable)
+        return islice(self.iterator, self.length)
 
     def __len__(self):
-        raise Exception('IterableSampler has no length')
+        if self.length is None:
+            raise Exception('Length is none')
+        else:
+            return self.length
