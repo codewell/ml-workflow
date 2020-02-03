b0d55dcc3c11f952926b45e42b1a07282d8ab6f9
Richard <samedii@gmail.com>
2020-02-03 10:50:43 +0100

    Remove imports

--- workflow/ignite/handlers/__init__.py
+++ workflow/ignite/handlers/__init__.py
@@ -4,5 +4,3 @@ from .add_evaluation_logger import add_evaluation_logger
 from .add_exponential_decay_lr import add_exponential_decay_lr
 from .add_lr_warmup import add_lr_warmup
 from .add_default_event_handlers import add_default_event_handlers
-from .add_train_progress_bar import add_train_progress_bar
-from .add_validation_progress_bar import add_validation_progress_bar
