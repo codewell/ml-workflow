9c799e31d7055fb20033b2954c9ba1eb7b92e82a
Richard <samedii@gmail.com>
2020-02-04 17:02:46 +0100

    Remove multiple evaluators. Cleanup. Fix bugs

--- workflow/ignite/handlers/add_default_event_handlers.py
+++ workflow/ignite/handlers/add_default_event_handlers.py
@@ -1,13 +1,10 @@
-import numpy as np
 import ignite
 from ignite.engine import Events
-from functools import partial
 from ignite.contrib.handlers.tensorboard_logger import (
     TensorboardLogger, OutputHandler, global_step_from_engine
 )
 
 from workflow.ignite.tqdm_print import tqdm_print
-from workflow.ignite.constants import TQDM_OUTFILE
 from .early_stopping import EarlyStopping
 from .epoch_logger import EpochLogger
 from .metrics_logger import MetricsLogger
@@ -16,33 +13,52 @@ from .progress_bar import ProgressBar
 
 
 def add_default_event_handlers(
-    model, optimizer, trainer, evaluators, validate_data_loader,
-    model_score_function, tensorboard_metric_names, config
+    model,
+    optimizer,
+    trainer,
+    validator,
+    validate_data_loader,
+    model_score_function,
+    trainer_metric_names,
+    validator_metric_names,
+    config,
 ):
-
-    if type(evaluators) != dict:
-        evaluators = dict(validating=evaluators)
 
     EpochLogger().attach(trainer)
 
     # Order of attaching progress bars is important for vscode / atom
     ProgressBar(desc='training').attach(
-        trainer,
-        output_transform=lambda output: dict(loss=output['loss']),
-        # ['running_loss'],
+        trainer, trainer_metric_names
     )
     MetricsLogger('training').attach(trainer)
+    TensorboardLogger(log_dir='tb').attach(
+        trainer,
+        OutputHandler(
+            tag='training',
+            metric_names=trainer_metric_names,
+        ),
+        Events.ITERATION_COMPLETED,
+    )
 
-    for evaluator_desc, evaluator in evaluators.items():
-        trainer.add_event_handler(
-            Events.EPOCH_COMPLETED,
-            lambda engine: evaluator.run(validate_data_loader)
-        )
+    trainer.add_event_handler(
+        Events.EPOCH_COMPLETED,
+        lambda engine: validator.run(validate_data_loader)
+    )
 
-        ProgressBar(desc=evaluator_desc).attach(evaluator)
-        MetricsLogger(evaluator_desc).attach(evaluator)
+    validator_desc = 'validating'
+    ProgressBar(desc=validator_desc).attach(validator)
+    MetricsLogger(validator_desc).attach(validator)
+    TensorboardLogger(log_dir='tb').attach(
+        validator,
+        OutputHandler(
+            tag=validator_desc,
+            metric_names=validator_metric_names,
+            global_step_transform=global_step_from_engine(trainer),
+        ),
+        Events.EPOCH_COMPLETED,
+    )
 
-    ModelCheckpoint(model_score_function).attach(evaluator, dict(
+    ModelCheckpoint(model_score_function).attach(validator, dict(
         model=model,
         optimizer=optimizer,
     ))
@@ -51,24 +67,4 @@ def add_default_event_handlers(
         Events.ITERATION_COMPLETED, ignite.handlers.TerminateOnNan(),
     )
 
-    TensorboardLogger(log_dir='tb').attach(
-        trainer,
-        OutputHandler(
-            tag='training',
-            output_transform=lambda output: dict(loss=output['loss']),
-        ),
-        Events.ITERATION_COMPLETED,
-    )
-
-    for evaluator_desc, evaluator in evaluators.items():
-        TensorboardLogger(log_dir='tb').attach(
-            evaluator,
-            OutputHandler(
-                tag=evaluator_desc,
-                metric_names=tensorboard_metric_names,
-                global_step_transform=global_step_from_engine(trainer),
-            ),
-            Events.EPOCH_COMPLETED,
-        )
-
-    EarlyStopping(model_score_function, trainer, config).attach(evaluator)
+    EarlyStopping(model_score_function, trainer, config).attach(validator)
