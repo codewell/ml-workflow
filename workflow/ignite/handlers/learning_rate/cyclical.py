51041ef822ad10ccc4206149c072762156d0e6a8
FelixAbrahamsson <felixab@kth.se>
2020-02-05 10:32:54 +0100

    Update cyclical.py

--- workflow/ignite/handlers/learning_rate/cyclical.py
+++ workflow/ignite/handlers/learning_rate/cyclical.py
@@ -8,7 +8,7 @@ class Cyclical(LinearCyclicalScheduler):
             param_name='lr',
             start_value=config['learning_rate'] / 100,
             end_value=config['learning_rate'],
-            cycle_size=config['steps_per_epoch'],
+            cycle_size=config['n_batches_per_epoch'],
             start_value_mult=config['lr_decay'],
             end_value_mult=config['lr_decay'],
         )
