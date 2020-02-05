be930aff6ee95b3b02f6bea4c14c5ff964a730a4
FelixAbrahamsson <felixab@kth.se>
2020-02-05 09:26:33 +0100

    Update add_warmup.py
    Now always expects the config parameter "warmup_duration" to be a float indicating the fraction of epochs for which warmup will last.

--- workflow/ignite/handlers/learning_rate/add_warmup.py
+++ workflow/ignite/handlers/learning_rate/add_warmup.py
@@ -2,20 +2,9 @@ from ignite.contrib.handlers.param_scheduler import create_lr_scheduler_with_war
 
 
 def add_warmup(lr_scheduler, config):
-
-    warmup_duration = (
-        config['warmup_duration'] if config['warmup_duration'] > 0
-        else config['n_batches_per_epoch'] * -config['warmup_duration']
-    )
-
-    warmup_end_value = (
-        config['warmup_end_value'] if config['warmup_end_value'] != -1
-        else config['learning_rate']
-    )
-
     return create_lr_scheduler_with_warmup(
         lr_scheduler,
-        config['warmup_start_value'],
-        warmup_end_value,
-        warmup_duration,
+        warmup_start_value=config['warmup_start_value'],
+        warmup_end_value=config['learning_rate'],
+        warmup_duration=int(config['n_batches_per_epoch'] * config['warmup_duration']),
     )
