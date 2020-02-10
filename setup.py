b283ea150bcee68151037fc69701bd7a9d317fd9
Felix Abrahamsson <felix@aiwizo.com>
2020-02-10 16:53:12 +0100

    Use PBR, change requirements

--- setup.py
+++ setup.py
@@ -2,18 +2,6 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='ml-workflow',
-    version='0.4.1',
-    description='Workflow tools for pytorch and ignite',
-    author='Felix Abrahamsson, Richard Löwenström, Jim Holmström',
-    author_email='richard@aiwizo.com',
-    keywords='pytorch ignite workflow utilities',
-    packages=find_packages(exclude=['venv', 'dist', 'recipes']),
-    install_requires=[
-        'torch>=1.2.0',
-        'pytorch-ignite>=0.3.0',
-        'tqdm>=4.41.1',
-        'opencv-python>=4.1.2.30',
-        'pandas>=0.25.3',
-    ],
+    setup_requires=['pbr'],
+    pbr=True,
 )
