6431a67ef81dfb968f0a572eaab4833aceb5f8f2
FelixAbrahamsson <felix@aiwizo.com>
2020-03-13 15:22:05 +0100

    Make default seed None

--- workflow/temporary_numpy_seed.py
+++ workflow/temporary_numpy_seed.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def temporary_numpy_seed(fn, seed):
+def temporary_numpy_seed(fn, seed=None):
     random_state = np.random.get_state()
     np.random.seed(seed)
     output = fn()
