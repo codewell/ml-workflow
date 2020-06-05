81e9eae1192e8bfcd95b6ba7fefcda305bf9413e
Richard <samedii@gmail.com>
2020-06-05 18:34:43 +0200

    Add soft binary cross entropy

---
+++ workflow/torch/binary_cross_entropy.py
@@ -0,0 +1,9 @@
+import torch
+
+
+def binary_cross_entropy(predicted, target):
+    '''https://github.com/tensorflow/tensorflow/blob/2b96f3662bd776e277f86997659e61046b56c315/tensorflow/python/ops/nn_impl.py#L113'''
+    return (
+        torch.relu(predicted) - predicted * target
+        + torch.where(predicted >= 0, -predicted, predicted).exp().log1p()
+    )
