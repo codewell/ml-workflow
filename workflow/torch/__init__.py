2c6de506f679658c167869230cf849572fc18c3a
Richard <samedii@gmail.com>
2020-08-24 10:23:01 +0200

    Requires grad context manager

--- workflow/torch/__init__.py
+++ workflow/torch/__init__.py
@@ -6,7 +6,7 @@ from workflow.torch.module_compose import ModuleCompose
 from workflow.torch.to_device import to_device
 from workflow.torch.to_shapes import to_shapes
 from workflow.torch.module_train import module_train, module_eval
-from workflow.torch.requires_nograd import requires_nograd
+from workflow.torch.requires_grad import requires_grad, requires_nograd
 from workflow.torch.set_learning_rate import set_learning_rate
 from workflow.torch.set_seeds import set_seeds
 
--- workflow/torch/requires_grad.py
+++ workflow/torch/requires_grad.py
@@ -2,19 +2,28 @@ from contextlib import contextmanager
 
 
 @contextmanager
-def requires_nograd(module):
+def requires_grad(module, grad=True):
     '''
     Usage:
-    with requires_nograd(model):
+    with requires_grad(model):
         prediction = model(features)
     '''
     requires_grads = [
         p.requires_grad for p in module.parameters()
     ]
     for p in module.parameters():
-        p.requires_grad = False
+        p.requires_grad = grad
 
     yield module
 
     for p, requires_grad in zip(module.parameters(), requires_grads):
         p.requires_grad = requires_grad
+
+
+def requires_nograd(module):
+    '''
+    Usage:
+    with requires_nograd(model):
+        prediction = model(features)
+    '''
+    return requires_grad(module, grad=False)
