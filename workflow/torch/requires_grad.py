a9dabb77aa772be6d83ed16cdbe32e12cb38c803
Jim Holmstrom <jim.holmstroem@gmail.com>
2020-08-27 20:14:11 +0200

    Removes the dependency of fixed order of nn.Module.parameters()

--- workflow/torch/requires_grad.py
+++ workflow/torch/requires_grad.py
@@ -9,20 +9,17 @@ class RequiresGrad:
 
         self.module = module
         self.grad = grad
-        self.requires_grads = [
-            p.requires_grad for p in module.parameters()
-        ]
+        self.named_requires_grads = {name: parameter.requires_grad for name, parameter in self.module.named_parameters()}
 
     def __enter__(self):
-        for p in self.module.parameters():
-            p.requires_grad = self.grad
+        for parameter in self.module.parameters():
+            parameter.requires_grad = self.grad
+
         return self.module
 
     def __exit__(self, type, value, traceback):
-        for p, requires_grad in zip(
-            self.module.parameters(), self.requires_grads
-        ):
-            p.requires_grad = requires_grad
+        for name, parameter in self.module.named_parameters():
+            parameter.requires_grad = self.named_requires_grads[name]
 
     def __call__(self, fn):
         @wraps(fn)
